c8acec31eb88f1174098a45e6c2ca5132dcb98e8
Suntisuk Khemphed <suntisuk.go@gmail.com>
2021-01-07 20:33:57 +0700

    [Edited]: Update Routes to Product List

--- sc-webstore/src/app/app-routing.module.ts
+++ sc-webstore/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { ProductListComponent } from './product-list/product-list.component';
+
+const routes: Routes = [
+  {
+    path: ''
+    , component: ProductListComponent
+  }
+  // ,{
+  //   path: 'product'
+  //   ,component: ProductItemComponent
+  // }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
