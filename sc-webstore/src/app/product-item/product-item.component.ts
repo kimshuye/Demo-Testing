7eb95c0cad83869064a6c6fa9098f6898694c024
Suntisuk Khemphed <suntisuk.go@gmail.com>
2021-01-07 19:39:08 +0700

    [Edited]: Pass data Product List to Product Item

--- sc-webstore/src/app/product-item/product-item.component.ts
+++ sc-webstore/src/app/product-item/product-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-product-item',
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-item.component.scss']
 })
 export class ProductItemComponent implements OnInit {
+
+
+  @Input() productItem;
 
   constructor() { }
 
--- sc-webstore/src/app/product-list/product-list.component.ts
+++ sc-webstore/src/app/product-list/product-list.component.ts
@@ -7,9 +7,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductListComponent implements OnInit {
 
+  products: any[];
+
+
   constructor() { }
 
   ngOnInit(): void {
+    this.products = [
+      {
+        id: 2,
+        name: '43 Piece dinner Set',
+        gender: 'Female',
+        age: '3 to 5',
+        price: 12.95,
+        quantity: 105,
+        currency: 'USD',
+        brand: 'CoolKidz',
+        image_url: 'https://media.takealot.com/covers_images/7d2a25e2dd0140a58ca47642a9999215/s-zoom.file'
+      }
+      , {
+        id: 1,
+        name: 'Balance Training Bicycle',
+        gender: 'Neutral',
+        age: '3 to 5',
+        price: 112.95,
+        quantity: 50,
+        currency: 'USD',
+        brand: 'SportsFun',
+        image_url: 'https://i.pinimg.com/originals/ab/7b/40/ab7b4094fb8e6674c19a82c03489edf0.jpg'
+      }
+    ];
   }
 
 }
