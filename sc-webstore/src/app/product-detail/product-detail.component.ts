35106845938f1ed6454c494d4d8fd59f21743004
Suntisuk Khemphed <suntisuk.go@gmail.com>
2021-01-08 15:10:09 +0700

    [Edited]: Product Detail Add onSubmit method

--- sc-webstore/src/app/product-detail/product-detail.component.ts
+++ sc-webstore/src/app/product-detail/product-detail.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
+import {NgForm} from '@angular/forms';
+
 @Component({
   selector: 'app-product-detail',
   templateUrl: './product-detail.component.html',
@@ -9,17 +11,23 @@ import { ActivatedRoute } from '@angular/router';
 export class ProductDetailComponent implements OnInit, OnDestroy {
   id: number;
   private sub: any;
-  productQuantity: number;
+  quantity: any;
 
   constructor(private route: ActivatedRoute) {}
 
   ngOnInit() {
-    this.productQuantity = 1;
+    this.quantity = 1;
     this.sub = this.route.params.subscribe(params => {
        this.id = +params.id; // (+) converts string 'id' to a number
 
        // In a real app: dispatch action to load the details here.
     });
+  }
+
+  onSubmit(productTemp: NgForm) {
+    console.log(productTemp.value);  // { first: '', last: '' }
+    console.log(productTemp.valid);  // false
+    console.log(this.quantity);  // false
   }
 
   ngOnDestroy() {
